888d760318cbbf966ca74503b05a7de49ff1dd7c
Amini <68004650+mamdollah@users.noreply.github.com>
2024-03-05 00:53:37 +0100

    functioning ResNet50 block-based feature extractor

--- feature-extraction_block-based.py
+++ feature-extraction_block-based.py
@@ -13,24 +13,25 @@ class Custom_Model(nn.Module):
         self.bn1 = rn50.bn1
         self.relu = rn50.relu
         self.maxpool = rn50.maxpool
-        rm50 = None
 
-        custom_layers = []
-        try:
-            for nb, layer in zip([3, 4, 6, 3], [rn50.layer1, rn50.layer2, rn50.layer3, rn50.layer4]):
-                if num_blocks > nb:
-                    custom_layers.extend(list(layer)[:nb])
-                    num_blocks -= nb
-                elif num_blocks > 0:
-                    custom_layers.extend(list(layer)[:num_blocks])
-                    num_blocks = 0
-        except ValueError:
-            print("num_blocks must be in [1,...,16]")
+        layers = [getattr(rn50, f"layer{i}") for i in range(1, 5)]
+        self.get_blocks(num_blocks, layers)
 
-        self.layers = nn.ModuleList(custom_layers)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
         self.freeze_params()
+        rn50 = None
+
+    def get_blocks(self, num_blocks, layers):
+        for i, layer in enumerate(layers, 1):
+            if not num_blocks: break
+            bottleneck_blocks = []
+            for bottleneck in layer:
+                if num_blocks:
+                    bottleneck_blocks.append(bottleneck)
+                    num_blocks -= 1
+                else:
+                    break
+            setattr(self, f"layer{i}", nn.Sequential(*bottleneck_blocks))
 
     def freeze_params(self):
         for param in self.parameters():
@@ -41,13 +42,12 @@ class Custom_Model(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
-        for layer in self.layers:
-            x = layer(x)
+        for seq in self.sequential_blocks:
+            x = seq(x)
         return self.avgpool(x)
 
 
-num_blocks = 3
+num_blocks = 5
 custom_model = Custom_Model(num_blocks)
 
 print(torchinfo.summary(custom_model))
-custom_model(torch.randn(1, 3, 224, 224)).shape
