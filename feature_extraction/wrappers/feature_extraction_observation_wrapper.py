7960c2ca62fadd969872ce1b70f7142e7e4f337b
Amini <68004650+mamdollah@users.noreply.github.com>
2024-03-19 14:52:13 +0100

    restructure repo

--- feature_extraction/wrappers/feature_extraction_observation_wrapper.py
+++ feature_extraction/wrappers/feature_extraction_observation_wrapper.py
@@ -1,18 +1,10 @@
 import gymnasium as gym
-import numpy as np
-from gymnasium.spaces import Box
-
-
-from feature_extraction.feature_extractors.stage_feature_extractor import StageFeatureExtractor
 
 
 class FeatureExtractionObservationWrapper(gym.ObservationWrapper):
-    def __init__(self, env):
+    def __init__(self, env, feature_extractor: BaseFeatureExtractor):
         super(FeatureExtractionObservationWrapper, self).__init__(env)
-        model_name = "microsoft/resnet-50"
-        stage = 1  # Example stage, adjust based on your model and requirements
-        self.feature_extractor = StageFeatureExtractor(model_name, stage)
-        print("Old observation space: ", self.observation_space)
+        self.feature_extractor = feature_extractor
         self.observation_space = self.feature_extractor.output_dim
 
     def observation(self, obs):
